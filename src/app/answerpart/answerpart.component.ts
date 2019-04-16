556216f7a7882695d7e0b42749138594ffa5b672
Carambis <lepeshko1997@mail.ru>
2019-04-16 21:59:41 +0300

    add result page add common answer page make font

---
+++ src/app/answerpart/answerpart.component.ts
@@ -0,0 +1,32 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {HttpService} from '../task/http.service';
+
+@Component({
+  selector: 'app-answerpart',
+  templateUrl: './answerpart.component.html',
+  styleUrls: ['./answerpart.component.css']
+})
+export class AnswerpartComponent implements OnInit {
+
+  @Input() questions: string[];
+  selectedValue: string;
+  @Input() openAnswer: boolean;
+  @Input() taskRest: string;
+
+  @Output() onchanged = new EventEmitter<boolean>();
+
+  change(openAnswer: boolean) {
+    this.onchanged.emit(openAnswer);
+    this.openAnswer = openAnswer;
+  }
+
+  constructor(private httpService: HttpService) {
+  }
+
+  ngOnInit() {
+  }
+
+  saveAnswer() {
+    this.httpService.addUserAnswer(this.taskRest, this.selectedValue);
+  }
+}
