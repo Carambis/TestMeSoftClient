6ef82bd078c6818088660fb754050126041aa7d0
Carambis <lepeshko1997@mail.ru>
2019-05-02 22:54:49 +0300

    fix auth fix answer fix text om main page fix header on all pages

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,34 +4,47 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {User} from '../user';
 import {Responce} from '../Responce';
 import {environment} from '../../environments/environment';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {MessageService} from 'primeng/api';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [MessageService]
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private router: Router, private httpClient: HttpClient) {
+  constructor(private router: Router,
+              private httpClient: HttpClient,
+              private fb: FormBuilder,
+              private messageService: MessageService) {
   }
 
+  userForm: FormGroup;
   user: User = new User();
 
   ngOnInit() {
+    this.userForm = this.fb.group({
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      groupNumber: new FormControl('', Validators.required)
+    });
   }
 
   startTask() {
-    this.httpClient.post(environment.baseUrl + 'user-service/login', this.user).subscribe((data: Responce) => {
-      sessionStorage.setItem('access_token', data.data);
-      this.httpClient.get(environment.baseUrl + 'task-service/startTest', {
-        headers: new HttpHeaders({
-          token: sessionStorage.getItem('access_token')
-        })
-      }).subscribe((value) => {
-          this.router.navigate(['/task']);
-        }
-      );
-    });
-
+    if (this.userForm.valid) {
+      this.httpClient.post(environment.baseUrl + 'user-service/login', this.user).subscribe((data: Responce) => {
+        sessionStorage.setItem('access_token', data.data);
+        this.httpClient.get(environment.baseUrl + 'task-service/startTest', {
+          headers: new HttpHeaders({
+            token: sessionStorage.getItem('access_token')
+          })
+        }).subscribe((value) => {
+            this.router.navigate(['/task']);
+          }
+        );
+      });
+    }
   }
 }
