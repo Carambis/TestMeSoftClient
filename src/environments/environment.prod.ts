49a485537c0f2240becc78d77ff37c27012c089f
Carambis <lepeshko1997@mail.ru>
2019-05-03 22:08:42 +0300

    local param

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://gatewaytestmesoft.mycloud.by/services/'
+  baseUrl: 'http://localhost:8080/services/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://gatewaytestmesoft.mycloud.by/services/'
+  baseUrl: 'http://localhost:8080/services/'
 };
 
 /*
